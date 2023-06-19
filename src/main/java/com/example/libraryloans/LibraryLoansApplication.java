8f03d8404db701f3c8bf7fba93d87abb3d89e734
Michael Ogundero <ogunderomichael@gmail.com>
2023-06-19 09:29:06 -0600

    first commit

---
+++ src/main/java/com/example/libraryloans/LibraryLoansApplication.java
@@ -0,0 +1,13 @@
+package com.example.libraryloans;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LibraryLoansApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LibraryLoansApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/libraryloans/LibraryLoansApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.libraryloans;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LibraryLoansApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
